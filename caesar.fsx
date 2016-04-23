80608e8062d19a1b2584cc1689db43d5b6ec052a
Mike Batanian <mikebatanian@gmail.com>
2016-04-23 16:46:36 -0400

    re-org the function to use nested functions rather than 'global' ones

--- caesar.fsx
+++ caesar.fsx
@@ -1,18 +1,18 @@
 // Basic implementation of a Caesar cipher
 
-let alphaLength = 26
-
-let shiftImpl (c: char) bnd shft =
-    let n = ((int c) - bnd + shft) % alphaLength + bnd
-    (char n)
-
-let shiftChar s c =
-    match System.Char.IsLetter(c) with
-    |   true when System.Char.IsUpper(c) -> shiftImpl c (int 'A') s
-    |   true when System.Char.IsLower(c) -> shiftImpl c (int 'a') s
-    |   _ -> c
-    
 let shiftWord w s =
+    let alphaLength = 26
+    
+    let shiftImpl (c: char) bnd shft =
+        let n = ((int c) - bnd + shft) % alphaLength + bnd
+        (char n)
+    
+    let shiftChar s c =
+        match System.Char.IsLetter(c) with
+        |   true when System.Char.IsUpper(c) -> shiftImpl c (int 'A') s
+        |   true when System.Char.IsLower(c) -> shiftImpl c (int 'a') s
+        |   _ -> c
+    
     let sc = shiftChar s
     String.map sc w
 
